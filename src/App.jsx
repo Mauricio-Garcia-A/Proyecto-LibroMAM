a3824a7043ff13ebe0a9720af4ae504107a9800e
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2024-08-17 10:34:08 -0300

    optimizacion de imagenes de png y jpg a webp, optimiza el rendimiento

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
       <section className='seccion-presentacion'>
       
         <header>
-          <img src='images/sombrero.png' className='imgSombrero' />
-          <img src='images/pulsera.png' className='imgPulsera' />
-          <img src='images/anteojos.png' className='imgAnteojos' />
+          <img src='images/webp/sombrero-v1.webp' className='imgSombrero' />
+          <img src='images/webp/pulsera-v1.webp' className='imgPulsera' />
+          <img src='images/webp/anteojos-v1.webp' className='imgAnteojos' />
         </header>
         <article>
           <div className='container-logo'>
@@ -20,13 +20,13 @@ function App() {
         </article>
         <footer>
           <div className='container-map'>
-            <img src='images/mapa.png' className='imgMap' />
-            <img src='images/planta1.png' className='imgPlanta1' />
-            <img src='images/birome.png' className='imgBirome' />
+            <img src='images/webp/mapa-v1.webp' className='imgMap' />
+            <img src='images/webp/planta1-v1.webp' className='imgPlanta1' />
+            <img src='images/webp/birome-v1.webp' className='imgBirome' />
           </div>
-          <img src='images/brujula.png' className='imgBrujula' />
-          <img src='images/camara.png' className='imgCamara' />
-          <img src='images/binoculares.png' className='imgBinoculares' />
+          <img src='images/webp/brujula-v1.webp' className='imgBrujula' />
+          <img src='images/webp/camara-v1.webp' className='imgCamara' />
+          <img src='images/webp/binoculares-v1.webp' className='imgBinoculares' />
 
         </footer>
       </section>
@@ -38,9 +38,9 @@ function App() {
         <Libro />
         </article>
         <footer className='seccion-libro-footer'>
-          <img src='images/mate.png' className='imgMate' />
+          <img src='images/webp/mate-v1.webp' className='imgMate' />
           <span>©Copyright 2024 - DIARIO DE UN CAMINO - M.A.G.</span>
-          <img src='images/termo.png' className='imgTermo' />
+          <img src='images/webp/termo-v1.webp' className='imgTermo' />
         </footer>
 
       </section>
