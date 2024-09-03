39735de83589532be4e0de9a7fc299f6dcba4002
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2024-09-03 00:41:31 -0300

    Se agrega un loader y una pagaina de presentacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.scss'
+import PagePresentacion from './pages/PagePresentacion/PagePresentacion';
 import PagePrincipal from './pages/PagePrincipal/PagePrincipal';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000); // Cambia el estado después de 5 segundos
+
+    return () => clearTimeout(timer); // Limpia el temporizador al desmontar
+  }, [])
+
   return (
     <>
-      <PagePrincipal />
+      {loading ? (
+        <PagePresentacion />
+      ) : (
+        <PagePrincipal />
+      )}
     </>
   )
 }
---
+++ src/pages/PagePresentacion/PagePresentacion.jsx
@@ -0,0 +1,10 @@
+import LoaderBook from '../../components/LoaderBook/LoaderBook'
+import './PagePresentacion.scss'
+
+export default function PagePresentacion() {
+  return (
+    <div className='container-PagePresentacion'>
+      <LoaderBook />
+    </div>
+  )
+}
